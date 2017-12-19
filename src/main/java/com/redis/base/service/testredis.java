f46ee352d4da18bc00e7c2cad4d1e504dca952d1
xiaoshulinppp <1321634605@qq.com>
2017-12-19 15:43:41 +0800

    分支增加和删除东西。。查看修改记录

--- src/main/java/com/redis/base/service/testredis.java
+++ src/main/java/com/redis/base/service/testredis.java
@@ -3,7 +3,12 @@ package com.redis.base.service;
 public class testredis {
 	//分支修改文件一；
 	public void fenzhi(){
-		System.out.println("分支方法");
 		
 	}
+	public void fenzhi2(){
+		System.out.println("新增分支方法2，删除方法一的内容");
+		
+	}
+	
+	
 }
